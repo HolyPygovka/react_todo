d6329521bef001154d33dd699020731827376593
unknown <r2598509@gmail.com>
2020-04-29 21:55:14 +0300

    les 3 restructering app

---
+++ src/components/SearchBlock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBlock = () => {
+
+    const searchBlockText = 'What are you want to search?'
+    return <input placeholder={searchBlockText} />;
+}
+
+export default SearchBlock;
